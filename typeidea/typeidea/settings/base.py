4011826d0054db1aa25b86078cc593f2703eef6f
zzjie871 <y5530961@163.com>
2019-05-31 22:30:06 +0800

    完成App和Model代码的创建

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
